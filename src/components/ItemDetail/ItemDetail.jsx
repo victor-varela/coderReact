81323eaec5af4954963d4bb53db0dbab4aeb96bf
Victor Varela <victor398@gmail.com>
2023-05-06 11:48:41 -0300

    commit

--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -1,72 +1,81 @@
-import { Card, CardHeader, CardBody, CardFooter, Stack, Heading, Text, Divider, Button, ButtonGroup, Image, Box } from '@chakra-ui/react'
-import { Counter } from '../Counter/Counter'
-import { useState } from 'react'
-import { Link } from 'react-router-dom'
+import {
+	Card,
+	CardHeader,
+	CardBody,
+	CardFooter,
+	Stack,
+	Heading,
+	Text,
+	Divider,
+	Button,
+	ButtonGroup,
+	Image,
+	Box,
+} from "@chakra-ui/react";
+import { Counter } from "../Counter/Counter";
+import {  useState } from "react";
+import { Link } from "react-router-dom";
+import { useCartContext } from "../../context/CartContext";
+
 export const ItemDetail = ({ item }) => {
-  const [isCant, setIsCant]= useState(false)
-  const onAdd=(cantidad)=>{
-    console.log("la cantidad seleccionada es:", cantidad)
-    setIsCant(true)
-  }
-  return (
-    <>
-      {!isCant ?
-        <Card maxW='sm' m="auto" align="center" >
-        <CardBody >
-          <Image
-            src={item.image}
-            alt=""
-            borderRadius='lg'
-          />
+					const [isCant, setIsCant] = useState(false);
+					const {addToCart}= useCartContext()
+					
+					const onAdd = (cantidad) => {
+						// { ...item, cantidad }
+						addToCart(item, cantidad ) 
+						setIsCant(true);
+					};
+					return (
+								<>
+									{!isCant ? (
+										<Card maxW="sm" m="auto" align="center">
+											<CardBody>
+												<Image src={item.image} alt="" borderRadius="lg" />
 
-          <Stack mt='6' spacing='3'>
-            <Heading size='md'>{item.title}</Heading>
-            <Text>
-              {item.description}
-            </Text>
-            <Text color='blue.600' fontSize='2xl'>
-              ${item.price}
-            </Text>
-          </Stack>
-        </CardBody>
-        <Divider />
-        <CardFooter>
-          <ButtonGroup spacing='2'>
-            
-            <Counter onAdd={onAdd}/>
-          </ButtonGroup>
-        </CardFooter>
-      </Card>
-      :
-      <>
-        <Card maxW='sm' m="auto" align="center" >
-        <CardBody >
-          <Image
-            src={item.image}
-            alt=""
-            borderRadius='lg'
-          />
+												<Stack mt="6" spacing="3">
+													<Heading size="md">{item.title}</Heading>
+													<Text>{item.description}</Text>
+													<Text color="blue.600" fontSize="2xl">
+														${item.price}
+													</Text>
+												</Stack>
+											</CardBody>
+											<Divider />
+											<CardFooter>
+												<ButtonGroup spacing="2">
+													<Counter onAdd={onAdd} />
+												</ButtonGroup>
+											</CardFooter>
+										</Card>
+									) : (
+										<>
+											<Card maxW="sm" m="auto" align="center">
+												<CardBody>
+													<Image src={item.image} alt="" borderRadius="lg" />
 
-          <Stack mt='6' spacing='3'>
-            <Heading size='md'>{item.title}</Heading>
-            <Text>
-              {item.description}
-            </Text>
-            <Text color='blue.600' fontSize='2xl'>
-              ${item.price}
-            </Text>
-          </Stack>
-        </CardBody>
-        <Divider />
-        <CardFooter>
-          <ButtonGroup spacing='2'>
-            <Link to="/cart">
-              <Button>Terminar Compra</Button>   
-            </Link>
-          </ButtonGroup>
-        </CardFooter>
-      </Card>
-    </>}
-    </>
-  )
-}
+													<Stack mt="6" spacing="3">
+														<Heading size="md">{item.title}</Heading>
+														<Text>{item.description}</Text>
+														<Text color="blue.600" fontSize="2xl">
+															${item.price}
+														</Text>
+													</Stack>
+												</CardBody>
+												<Divider />
+												<CardFooter>
+													<ButtonGroup spacing="2">
+														<Link to="/cart">
+															<Button>Terminar Compra</Button>
+														</Link>
+														<Link to="/">
+															<Button>Seguir Comprando</Button>
+														</Link>
+													</ButtonGroup>
+												</CardFooter>
+											</Card>
+										</>
+									)}
+								</>
+							);
+				};
