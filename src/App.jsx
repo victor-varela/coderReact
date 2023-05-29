7e6986116076f8c44423fe2bd549588168c5e562
Victor Varela <victor398@gmail.com>
2023-05-29 08:55:45 -0300

    last commit

--- src/App.jsx
+++ src/App.jsx
@@ -14,16 +14,19 @@ import Footer  from "./components/Footer/Footer";
 
 
 function App() {
+  const routes=[
+      {id:"1", path:"/", element:<ItemlistContainer greeting={"HOME"}/>},
+      {id:"2", path:"/categoria/:cid", element:<ItemlistContainer/>},
+      {id:"3", path:"/detail/:pid", element:<ItemDetailContainer/>},
+      {id:"4", path:"*", element:<Navigate/>},
+      {id:"5", path:"/cart", element:<CartContainer/>}
+  ]
   return (
     <CartContextProvider >
       <Router>
         <NavBar />
         <Routes>
-          <Route path="/" element={<ItemlistContainer greeting={"HOME"} />} />
-          <Route path="/categoria/:cid" element={<ItemlistContainer />} />
-          <Route path="/detail/:pid" element={<ItemDetailContainer />} />
-          <Route path="*" element={<Navigate to="/" />} />
-          <Route path="/cart" element={<CartContainer />} />
+          {routes.map(route=><Route key={route.id} path={route.path} element={route.element}/>)}
         </Routes>
         <Footer />
       </Router>
